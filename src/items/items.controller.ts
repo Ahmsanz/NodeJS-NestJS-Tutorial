941a797c1430824889648d3c1be5ffdc5455fde4
ahmsanz <adrian.h.m.sanz@gmail.com>
2020-06-05 16:37:03 +0200

    all fake routes set, items service created

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,16 +1,32 @@
-import { Controller, Get, Post, Put, Delete, Body, Req, Res } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create_item_dto';
-import { Request, Response } from 'express'; 
+ 
 
 @Controller('items')
 export class ItemsController {
     @Get()
     findAll(): string {
-        return 'Get all items'; 
+        return 'Getting all items'
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id): string {
+        return `Item ${id}`
     }
 
     @Post()
     create(@Body() createItemDto: CreateItemDto): string{
         return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`
+    }
+
+    @Put(':id')
+    update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+        return `Update ${id} -- Name: ${updateItemDto.name}`
+    }
+
+
+    @Delete(':id')
+    delete(@Param('id') id): string {
+        return `Delete ${id}`
     }
 }
